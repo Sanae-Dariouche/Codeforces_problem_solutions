4dd704a38a34b42df1d9e0252a79dfcab66842d8
Sanae <sanaedariouche@gmail.com>
2018-11-13 18:15:40 +0000

    Johny Likes Numbers import java.util.Scanner;

---
+++ C.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class C{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int k=sc.nextInt();
+		int x;
+		if(k>n) 
+		System.out.println(k);
+	else {
+		x=k*(n/k)+k;
+		System.out.println(x);
+	}
+	}
+}
