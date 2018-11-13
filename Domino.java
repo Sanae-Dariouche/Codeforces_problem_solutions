646c7fdf1c8bde358ea164425bc8fdf1a312f862
Sanae <sanaedariouche@gmail.com>
2018-11-13 18:08:16 +0000

    Domino piling import java.util.*;

---
+++ Domino.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Domino {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int M=sc.nextInt();
+		int N=sc.nextInt();
+		System.out.println((int)(M*N)/2);
+	}
+
+}
